239458e463b7d5232614b3272e15decc05bc510b
Evgeniy Krupin <krea@inbox.ru>
2022-02-20 14:43:15 +0700

    Database table meals Meal class JdbcMealRepository

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS user_roles;
+DROP TABLE IF EXISTS meals;
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
@@ -23,3 +24,15 @@ CREATE TABLE user_roles
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    description      VARCHAR                           NOT NULL,
+    datetime         TIMESTAMP           DEFAULT now() NOT NULL,
+    calories         INTEGER                           NOT NULL,
+    user_id          INTEGER                           NOT NULL,
+    CONSTRAINT  meal_datetime_idx UNIQUE (user_id, datetime),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+CREATE INDEX detetime_desc_idx ON meals(datetime DESC);
